5e3974fa2ce3f6187a060345addc0c21aac1375f
Dusan Radojcic <dusan.radojcic@htecgroup.com>
2023-09-13 02:00:34 +0200

    Add resilience4j

--- order-service/src/main/java/com/dipl/order/feign/InventoryFeignClient.java
+++ order-service/src/main/java/com/dipl/order/feign/InventoryFeignClient.java
@@ -2,6 +2,8 @@ package com.dipl.order.feign;
 
 import com.dipl.order.feign.dto.DecreaseInventoryRequest;
 import com.dipl.order.feign.dto.ItemDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +18,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface InventoryFeignClient {
 
   @GetMapping
+  @Retry(name = "inventory")
+  @CircuitBreaker(name = "inventory", fallbackMethod = "getItemsFallback")
   List<ItemDto> getItems(@RequestParam("ids") List<Long> ids);
+
+  default List<ItemDto> getItemsFallback(
+      List<Long> ids, RuntimeException exception) {
+
+    throw new RuntimeException("Inventory service is currently unavailable", exception);
+  }
+
 
   @PostMapping("/decrease")
   void decreaseQuantity(@RequestBody DecreaseInventoryRequest request);
